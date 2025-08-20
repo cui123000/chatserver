97b978df05cee5781ec2dce1049878629b1d3da1
xuan <2997573803@qq.com>
2025-08-20 15:54:32 +0800

    优化

---
+++ build/src/client/CMakeFiles/ChatClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/xuan/桌面/CHAT/chatserver/bin/ChatClient"
+  "/home/xuan/桌面/CHAT/chatserver/bin/ChatClient.pdb"
+  "CMakeFiles/ChatClient.dir/main.cpp.o"
+  "CMakeFiles/ChatClient.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
